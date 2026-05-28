acc086e574c513b8786e10e025e749d46ebd8a39
reychelbogantes <reychelfallasbogantes@gmail.com>
2026-05-28 16:30:11 -0600

    eliminacion de endpoint para crear super usuario

--- baseDatosVC/baseDatosVC/urls.py
+++ baseDatosVC/baseDatosVC/urls.py
@@ -1,19 +1,25 @@
+"""
+URL configuration for baseDatosVC project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from django.http import JsonResponse
-from django.contrib.auth import get_user_model
-
-def crear_super(request):
-    User = get_user_model()
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@admin.com', 'Admin1234!')
-        return JsonResponse({'ok': 'Superusuario creado'})
-    return JsonResponse({'ok': 'Ya existe'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('crear-super/', crear_super),
     path("", RedirectView.as_view(url="/api/", permanent=False)),
 ]
