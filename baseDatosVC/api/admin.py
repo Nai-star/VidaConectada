f266d477c82a71762de6d8d108b3506fc20c4cc1
reychelbogantes <reychelfallasbogantes@gmail.com>
2026-06-02 18:38:55 -0600

    Cambio en admin.py

--- baseDatosVC/api/admin.py
+++ baseDatosVC/api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from .models import Sangre, Urgente_Tip_Sang, Provincia, Cantones
 
 admin.site.register(User)
-
-# Register your models here.
+admin.site.register(Sangre)
+admin.site.register(Urgente_Tip_Sang)
+admin.site.register(Provincia)
+admin.site.register(Cantones)
